73592bef09b61b09f27eaeb1bebe531507947958
Gabriel Salviete <gsalviete@gmail.com>
2026-02-26 22:06:08 -0300

    feat: zod validation scheme

---
+++ src/accounts/account.schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const CreateAccountSchema = z.object({
+  id: z.string().uuid().optional(),
+  name: z.string().optional(),
+  direction: z.enum(['debit', 'credit']),
+  balance: z.number().optional().default(0)
+})
+
+export type CreateAccountDTO = z.infer<typeof CreateAccountSchema>
