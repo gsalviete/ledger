cca384ee772c5d69c3a319fa842911dcdc5f4eba
Gabriel Salviete <gsalviete@gmail.com>
2026-03-04 20:51:24 -0300

    fix: add interface account handling

--- src/accounts/inmemory.account.repository.ts
+++ src/accounts/inmemory.account.repository.ts
@@ -5,12 +5,18 @@ export class InMemoryAccountRepository implements AccountRepository {
     private accounts: Account[] = []
 
     async save(account: Account): Promise<Account>{
+        const existingId = await this.findById(account.id)
+        if(existingId){
+            throw new Error(`Id ${account.id} já existe.`)
+        }
         this.accounts.push(account)
         return account
+        
+        
     }
 
     async findById(id: string): Promise<Account | undefined> {
         const account = this.accounts.find(a => a.id === id)
-        return account ?? undefined;
+        return account;
     }
 }
